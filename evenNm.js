15705a3156ed9aa06b9b2640cbd8cab25f5838cd
coder girl <56164343+kareey-commits@users.noreply.github.com>
2020-07-04 16:17:33 +0300

    made changes

--- evenNm.js
+++ evenNm.js
@@ -1,8 +1,8 @@
-function checkNumber(){
-    var no;
+//a function that checks whether a number is odd or even
+function checkNumber(no){
     if(no % 2 == 0){
-        alert("This an even number");
-    } else{
-        alert("This is an odd number");
+        return("This an even number");
     }
-}
+        return("This is an odd number");
+    }
+    console.log(checkNumber(10));
