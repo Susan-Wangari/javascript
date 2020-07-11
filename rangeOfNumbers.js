b47b57fbcb12adc304330c6666bd6607a5f3beb7
kareey-commits <56164343+kareey-commits@users.noreply.github.com>
2020-07-11 21:31:27 +0300

    recursion to get a range of numbers

---
+++ rangeOfNumbers.js
@@ -0,0 +1,11 @@
+//using recursion to get a range of numbers
+function rangeOfNumbers(startNum, endNum) {
+    if (endNum - startNum === 0) {
+      return [startNum];
+    } else {
+      var numbers = rangeOfNumbers(startNum, endNum - 1);
+      numbers.push(endNum);
+      return numbers;
+    }
+  };
+  
