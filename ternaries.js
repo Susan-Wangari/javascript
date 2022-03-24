294c39b549b19dbbfd4b68dc6c32fa71b6a6ee6e
Susan Wangari <56164343+Susan-Wangari@users.noreply.github.com>
2022-03-24 17:01:18 +0300

    ternary expressions

---
+++ ternaries.js
@@ -0,0 +1,13 @@
+//if else
+let greeting;
+
+if (age < 10) {
+  greeting = "Hey there"; 
+} else {
+  greeting = "That's an interesting age!";  
+}
+console.log(greeting);
+
+//ternary operator/expression
+const greeting = age < 10 ? "Hey there" : "That's an interesting age!";
+console.log(greeting);
