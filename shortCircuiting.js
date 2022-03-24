c4e9065ae75526c35b3741a563983c1049c3e510
Susan Wangari <56164343+Susan-Wangari@users.noreply.github.com>
2022-03-24 18:31:12 +0300

    short circuiting

--- shortCircuiting.js
+++ shortCircuiting.js
@@ -1,5 +1,9 @@
 //ternary expression
 const username = response ? response : "guest";
 
+console.log(username);
+
 //short circuiting
 const username = response || "guest";
+
+console.log(username);
