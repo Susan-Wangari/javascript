1e174e305184ad9e9e10c198e9ebacbdbb998513
Susan Wangari <56164343+Susan-Wangari@users.noreply.github.com>
2022-03-24 16:34:12 +0300

    does not change a state of a value

---
+++ tripleEquals.js
@@ -0,0 +1,6 @@
+ if (null === undefined) {
+  console.log('equals');
+ } else {
+   console.log('not equals');
+}
+//will not change the null to be equal to undefined as compared to the loose equals operator(==)
