14efb010b5c4cb5b055e07a2f598ce78b0dd2fd3
Susan Wangari <56164343+Susan-Wangari@users.noreply.github.com>
2022-04-02 14:29:52 +0300

    arrow functions

---
+++ arrowFunction.js
@@ -0,0 +1,7 @@
+//normal function
+const capitalize = function(name) {};
+
+//arrow function
+const capitalize = (name)  => {};
+
+//does not use the function keyword and has an arrow after the parameters
