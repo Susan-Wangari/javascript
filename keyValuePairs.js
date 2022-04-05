7725ca1d949314e8001707602d3effb4a4374dda
Susan Wangari <56164343+Susan-Wangari@users.noreply.github.com>
2022-04-05 23:18:18 +0300

    object key value pairs

---
+++ keyValuePairs.js
@@ -0,0 +1,13 @@
+const blue = '#00f';
+const orange = '#f60';
+
+// variables - boxes; objects - file cabinets
+//shortened
+const colors = { yellow: '#ff0', blue, orange };
+//full 
+const colors = { 
+  yellow: '#ff0', 
+  blue: blue,
+  orange: orange };
+// objectName.key
+console.log(colors.blue);
