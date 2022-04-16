8f70d5935adc2a8868144bb6332bfec7179d2569
Susan Wangari <56164343+Susan-Wangari@users.noreply.github.com>
2022-04-16 17:31:01 +0300

    dot notation to access objects

---
+++ dotNotation.js
@@ -0,0 +1,13 @@
+const colors = {
+  'yellow Color': '#ff0',
+  blue: "#f00",
+  orange: "#f60"
+};
+
+//used to add and modify an object property
+colors.red = '#foo';
+
+colors.red = '#f00';
+
+console.log(colors);
+
