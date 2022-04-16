b52c58bcc906242507796932ab0342ad288e0ccb
Susan Wangari <56164343+Susan-Wangari@users.noreply.github.com>
2022-04-16 20:12:38 +0300

    destructuring with objects and functions

---
+++ deconstruction.js
@@ -0,0 +1,33 @@
+// Challenge: The recommendations object contains a set of of nice places to visit in Brighton, UK,
+// organized by what you'd like to do (eat pancakes, drink coffee etc).
+
+// 1. Destructure the places to drink (coffee and beer) from recommendations
+// 2. Destructure the places to listen to music
+// 3. Write a function that takes the recommendations object as an argument and that
+//      a) Logs out the music venues in recommendations when invoked 
+//      b) Uses object descructuring to get the "traditional" and "jazz" keys from the argument
+
+const recommendations = {
+    pancakes: 'Nowhere Man',
+    riceBowls: 'Pompoko',
+    beer: 'The Craft Beer Co.',
+    coffee: 'Coffee Roasters',
+    small_plates: 'Venetian Plates',
+    music: { 
+        traditional: 'Fiddler\'s Elbow', 
+        jazz: 'The Paris House'
+    }
+}
+ const  {beer, coffee}=recommendations;
+    console.log(`Places you can have drinks are: ${beer} and ${coffee}`);  
+
+ 
+ const {music: {jazz}} = recommendations;
+     console.log(`Here are some of the places you can listen to jazz music: ${jazz}`);
+
+//function that takes the object as an argument
+function placesToVisit ({music: {jazz, traditional}}) {
+    console.log(`Here are some of the places you can listen to music: ${jazz}, ${traditional}`);
+}
+placesToVisit(recommendations);
+
