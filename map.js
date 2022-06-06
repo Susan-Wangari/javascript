fda40333e241fe09d6e36e1a863c05f9f89869ae
Susan Wangari <56164343+Susan-Wangari@users.noreply.github.com>
2022-06-06 15:28:44 +0300

    using map in place of objects

---
+++ map.js
@@ -0,0 +1,23 @@
+//object
+// Challenge: 
+// 1. Take the object (contains a favourite place in Brighton, UK), and turn it into a Map
+// 2. Add a boolean property 'visited', to indicate places that you've been to it
+// 3. Add an integer property 'averageBill' with how much you spend there on average
+// 4. Fetch one of the properties using the get() method
+
+//object
+const favouritePlace = {
+    music: "jazz",
+    name: "Paris House"
+};
+
+//map
+const myPlaces = new Map([
+    ['music', 'jazz'],
+    ['name', 'Paris House'],
+    ['visited' , true],
+    ['averageBill', 25]
+]);
+
+myPlaces.get("music");
+
