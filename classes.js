3e66b88f18595613203b89b525017e0bb449a490
Susan Wangari <56164343+Susan-Wangari@users.noreply.github.com>
2022-10-26 15:38:43 +0300

    create a sample of a class

---
+++ classes.js
@@ -0,0 +1,8 @@
+class Surgeon {
+  constructor (name,department) {
+    this.name= name;
+    this.department=department;
+  }
+
+};
+//a class uses a constructor. The constructor uses this which refers to the instance of that class
